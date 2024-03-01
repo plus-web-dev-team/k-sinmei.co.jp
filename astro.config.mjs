61180ad733d8c5e99b4073f9ce43549cbf74d698
plus-agc-service-yamauchi <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2024-03-01 15:57:21 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [relativeLinks()],
+  integrations: [relativeLinks()],
+  build: {
+    site: "https://k-sinmei.co.jp",
+    inlineStylesheets: "never",
+    assets: "assets",
+    format: "file",
+  },
+  vite: {
     build: {
-        site: 'https://k-sinmei.co.jp',
-        inlineStylesheets: 'never',
-        assets: 'assets',
-        format: 'file',
-    },
-    vite: {
-        build: {
-            rollupOptions: {
-                output: {
-                    assetFileNames: "[ext]/[name][extname]",
-                },
-            },
+      rollupOptions: {
+        output: {
+          assetFileNames: "[ext]/[name][extname]",
         },
+      },
     },
+  },
+  output: "server",
+  adapter: netlify(),
 });
