c54fc8394af39909ba6328d5f8464f55d153862e
plus-agc-service-yamauchi <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2024-03-04 17:38:25 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import relativeLinks from "astro-relative-links";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [relativeLinks()],
   output: "server",
   adapter: netlify(),
 });
