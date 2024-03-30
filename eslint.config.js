644f0440d8c506ad6cc31073ed54133ea6ff1d66
NADARAKAinc <info@nadaraka.com>
2024-03-30 12:07:55 +0900

    modified eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,23 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs['flat/recommended'],
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    }
+module.exports = {
+    extends: [
+      "plugin:astro/recommended",
+    ],
+    overrides: [
+      {
+        // Define the configuration for `.astro` file.
+        files: ["*.astro"],
+        // Allows Astro components to be parsed.
+        parser: "astro-eslint-parser",
+        // Parse the script in `.astro` as TypeScript by adding the following configuration.
+        // It's the setting you need when using TypeScript.
+        parserOptions: {
+          parser: "@typescript-eslint/parser",
+          extraFileExtensions: [".astro"],
+        },
+        rules: {
+          // override/add rules settings here, such as:
+          // "astro/no-set-html-directive": "error"
+        },
+      },
+    ],
   }
-];
